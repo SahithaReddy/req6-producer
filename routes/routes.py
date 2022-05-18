107f33b7077ab491a60427d97ddb397c3670137d
SahithaReddy <sahithareddy2000@gmail.com>
2022-05-18 16:45:25 +0530

    first-commit

--- routes/routes.py
+++ routes/routes.py
@@ -5,7 +5,7 @@ from producer import messageSender
 
 @app.route('/upload',methods=['POST'])
 def upload_image():
-    bucket='mybuck65'
+    bucket='terraform817504'
     content_type=request.mimetype
     obj=request.files['file']
     filename=obj.filename
